e1b95aa2a33e6159e19e97652e816186bfc5352a
Zach Josephson <zjosephson@gmail.com>
2013-10-19 10:16:13 -0700

    random stuff

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-drop table if exists posts;
-create table posts (
-	id integer primary key autoincrement,
-	title text not null,
-	datePublished DateTime not null,
-	text text not null,
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  datePublished DateTime,
+  text text not null
 );
