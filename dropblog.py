d4a4b1e88e4c1c11ce4648714f30c283f13a6ad1
Zach Josephson <zjosephson@gmail.com>
2013-10-20 13:04:09 -0700

    modified CSS to look better.  Added date option to main page. Added links to post list display

--- dropblog.py
+++ dropblog.py
@@ -45,29 +45,35 @@ def show_entries():
 	fileNames = []
 	postText = []
 	postURL = []
+	dates = []
 	for line in blogPosts:
 		if not line.startswith('#'):
 			postURL.append(baseurl + line.strip())
-			fileNames.append(line.strip())
+			fileNames.append('/post/' + line.strip())
+	postURL = postURL[::-1]
+	fileNames = fileNames[::-1]
 	blogPosts.close()
 	for i in postURL:
 		post = urllib.urlopen(i)
 		result = ""
 		title = i[49:]
 		title = os.path.splitext(title)[0]
+		date = ""
 		for line in post:
 			if line.startswith("<title>"):
 				title = line[7:]
+			elif line.startswith("<date>"):
+				date = line[6:]
 			else:
 				result += line + "\n"
 		titles.append(title)
+		dates.append(date)
 		result = markdown2.markdown(result)
 		postText.append(result)
 		post.close()
 
 
-
-	entries = [dict(title=titles[i], text=postText[i], url=fileNames[i]) for i in range(len(titles))]
+	entries = [dict(title=titles[i], text=postText[i], url=fileNames[i], date=dates[i]) for i in range(len(titles))]
 	return render_template('show_entries.html', entries=entries)
 
 @app.route('/post_list')
@@ -77,9 +83,13 @@ def show_list():
 	titles = []
 	postText = []
 	postURL = []
+	fileNames = []
 	for line in blogPosts:
 		if not line.startswith('#'):
 			postURL.append(baseurl + line.strip())
+			fileNames.append('/post/' + line.strip())
+	postURL = postURL[::-1]
+	fileNames = fileNames[::-1]
 	blogPosts.close()
 	for i in postURL:
 		post = urllib.urlopen(i)
@@ -92,9 +102,11 @@ def show_list():
 		titles.append(title)
 		post.close()
 
-	return render_template('post_list.html', titles=titles)
+	entries = [dict(title=titles[i], fileName=fileNames[i]) for i in range(len(titles))]
 
-@app.route('/<postTitle>')
+	return render_template('post_list.html', entries=entries)
+
+@app.route('/post/<postTitle>')
 def single_post(postTitle):
 	baseurl = 'https://dl.dropboxusercontent.com/u/9366248/blog/'
 	post = urllib.urlopen(baseurl + postTitle)
